bfcad0e71b8bac74e4399521d199628c1425b892
Claire <clairebrady12@hotmail.com>
2024-12-18 23:15:50 +0000

    added food

--- js/app.js
+++ js/app.js
@@ -2,28 +2,34 @@
 const gridRows = 20
 const gridColumns = 20
 const totalCellCount = gridRows * gridColumns
-
+const gridCells = []
 
 //-------------------------Variables---------------------- 
-//let currentPosition = startPosition
+
 let score = 0
 let snakePositions = [108, 128, 148]
 let snakeDirection = 20
 
-//-------------------------Elements----------------------
-//const startButtonElement = document.querySelector(".start-game")
-const gridContainer = document.querySelector('.grid')
-const scoreElement = document.querySelector(".score")
-const gridCells = []
-//const snakeElement = document.querySelector('.snake')
 
-//const allCellsElement = sdocument.querySelectorAll(".cell")
-//const snakeEls = document.querySelectorAll('.cell')
+
+//-------------------------Elements----------------------
+
+const gridContainer = document.querySelector('.grid')
+const scoreElement = document.querySelector('.score')
+const foodElement = document.querySelector('.food')
+
+
+
+
+//-------------------------Functions--------------------
 
 generateBox()
 startGame()
+// moveFood()
+//addFood()
+//eatFood()
+// eatFood()
 
-//-------------------------Functions--------------------
 function generateBox() {
     for (let idx = 0; idx < totalCellCount; idx++) {
         const cell = document.createElement('div')
@@ -33,13 +39,17 @@ function generateBox() {
         cell.style.height = `${100 / gridRows}%`
         gridContainer.appendChild(cell)
         gridCells.push(cell)
-
     }
-  
     addSnake()
 
 }
 
+
+//-------------------extraElements after page load-----------------
+const allCellsElement = document.querySelectorAll('.cell')
+let randomCellIndex = Math.floor(Math.random()* allCellsElement.length)
+addFood()
+//-------------------------Functions continued--------------------
 
 
 
@@ -49,15 +59,12 @@ function addSnake() {
     })
 }
 
+
 function removeSnake() {
     snakePositions.forEach(snakePositionIdx => {
         gridCells[snakePositionIdx].classList.remove('snake')
     })
 }
-
-
-
-//--------------------------Events---------------------
 
 function changeSnakeDirection(event) {
     const pressedKey = event.code
@@ -65,7 +72,6 @@ function changeSnakeDirection(event) {
      if ( pressedKey === 'ArrowLeft'){
         snakeDirection = -1
      }
-
      else if ( pressedKey === 'ArrowRight'){
         snakeDirection = 1
      }
@@ -78,11 +84,6 @@ function changeSnakeDirection(event) {
 }
 
 
-document.addEventListener('keydown', changeSnakeDirection)
-
-
-//-----------------------interval -----------------------
-
 function moveSnake() {
     snakePositions.pop()
     snakePositions.unshift(snakePositions[0] + snakeDirection)
@@ -90,11 +91,72 @@ function moveSnake() {
 
 function startGame() {
     setInterval(() => {
+       
         removeSnake()
         moveSnake()
         addSnake()
         console.log(snakePositions)
+        
     }, 2000)
+    
 }
-//-----------event listeners----------------
+
+
+
+console.log(randomCellIndex)
+console.log(allCellsElement)
+
+// eatFood()
+//allCellsElement[randomCellIndex].classList.add('food')
+
+console.log(allCellsElement[randomCellIndex])
+
+// function moveFood(){
+//     removeFood()
+//     let randomCellIndex = Math.floor(Math.random()* allCellsElement.length)
+//     addFood(randomCellIndex)
+//     console.log(randomCellIndex)
+// }
+
+
+// function addFood(randomCellIndex){
+//     allCellsElement[randomCellIndex].classList.add('food')
+// }
+
+const food = document.querySelector(".food")
+//const cellElement = document.querySelector(".cell")
+
+// function removeFood(){
+//     if (allCellsElement[randomCellIndex].classList.contains('snake')){
+//         food.classList.remove("food")
+//     }}
+
+
+    function addFood() {
+        let randomCellIndex = Math.floor(Math.random()* allCellsElement.length)
+        allCellsElement[randomCellIndex].classList.add('food')
+        }
+    
+
+
+// function eatFood(){
+//     //addFood()
+//     let randomCellIndex = Math.floor(Math.random()* allCellsElement.length)
+//     if (allCellsElement[randomCellIndex].classList.contains('snake')){
+//     removeFood()}
+// //increment score by 5
+
+//     }
+
+
+// function eatFood(){
+//     addFood()
+//     removeFood()
+// }
+
+//--------------------------Event Listeners---------------------
+
+document.addEventListener('keydown', changeSnakeDirection)
+
+
 
